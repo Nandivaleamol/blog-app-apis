b82e0bf807d4e9ddfd4d54ac20381da201c5b758
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-19 11:50:25 +0530

    Post service added

--- src/main/java/com/blog/app/entities/Category.java
+++ src/main/java/com/blog/app/entities/Category.java
@@ -1,10 +1,16 @@
 package com.blog.app.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;import org.springframework.context.annotation.Description;
 
 import lombok.Getter;
@@ -25,6 +31,9 @@ public class Category {
 	private String categoryTitle;
 	@Column(name = "description", length = 5000)
 	private String categoryDescription;
+	
+	@OneToMany(mappedBy = "category",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 	
 
 }
--- src/main/java/com/blog/app/entities/User.java
+++ src/main/java/com/blog/app/entities/User.java
@@ -1,11 +1,17 @@
 package com.blog.app.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.processing.Generated;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,5 +30,8 @@ public class User {
 	private String email;
 	private String about;
 	private String password;
+	
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 
 }
